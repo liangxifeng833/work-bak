c7b61f84bdd1e79f28799a67902549cbcd02f9a5
liangxifeng <liangxifeng833@163.com>
2020-12-20 17:53:53 +0800

    add new file

---
+++ mm/补贴商品.mm
@@ -0,0 +1,30 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1608434615298" ID="ID_815901925" MODIFIED="1608434623223" TEXT="&#x8865;&#x8d34;&#x5546;&#x54c1;">
+<node CREATED="1608434644179" ID="ID_1287268052" MODIFIED="1608434649935" POSITION="right" TEXT="&#x6280;&#x672f;&#x70b9;&#xff1a;">
+<node CREATED="1608434691862" ID="ID_845737163" MODIFIED="1608434715604" TEXT="&#x524d;&#x7aef;&#x76f4;&#x63a5;ajax&#x8bf7;&#x6c42;&#x9886;&#x57df;&#x5c42;&#xff0c;&#x9886;&#x57df;&#x5c42;&#x89e3;&#x51b3;&#x8de8;&#x57df;"/>
+<node CREATED="1608434717021" ID="ID_1214868811" MODIFIED="1608434788499" TEXT="js &#x8bf7;&#x6c42;&#x9886;&#x57df;&#x5c42;&#x63a5;&#x53e3;&#x524d;&#xff0c;&#x5148;&#x7edf;&#x4e00;&#x8bf7;&#x6c42;&#x67b6;&#x6784;&#x6a21;&#x5757;&#xff0c;&#x9700;&#x8981;&#x5c01;&#x88c5;&#x5355;&#x72ec;&#x7684;js&#x6587;&#x4ef6;"/>
+</node>
+<node CREATED="1608435432279" ID="ID_1643832942" MODIFIED="1608435435667" POSITION="right" TEXT="&#x95ee;&#x9898;&#xff1a;">
+<node CREATED="1608435436470" ID="ID_753151874" MODIFIED="1608435451265" TEXT="&#x4e0b;&#x5355;&#x7cfb;&#x7edf; &#x5bfc;&#x822a;&#x56fe;&#x6807;&#x8c01;&#x6765;&#x505a;&#xff1f; "/>
+</node>
+<node CREATED="1608434792583" ID="ID_1626677224" MODIFIED="1608434841730" POSITION="right" TEXT="&#x63a5;&#x53e3;">
+<node CREATED="1608434796125" ID="ID_256269546" MODIFIED="1608434869211" TEXT="&#x5546;&#x54c1;&#x9886;&#x57df;&#x5c42;(domain_product)">
+<node CREATED="1608434869973" ID="ID_1974025986" MODIFIED="1608434876617" TEXT="&#x9009;&#x62e9;&#x8865;&#x8d34;&#x5546;&#x54c1;">
+<node CREATED="1608455856608" ID="ID_176599804" MODIFIED="1608455857221" TEXT="https://docs.qq.com/doc/DSmV3dUthSUtISHBa"/>
+</node>
+</node>
+<node CREATED="1608434883356" ID="ID_1373954252" MODIFIED="1608434907142" TEXT="&#x6536;&#x94f6;&#x9886;&#x57df;&#x5c42;(domain_payment)">
+<node CREATED="1608434908278" ID="ID_1425368938" MODIFIED="1608434909705" TEXT="&#x63d0;&#x4ea4;&#x8865;&#x8d34;&#x5546;&#x54c1;">
+<node CREATED="1608455859749" ID="ID_1205552951" MODIFIED="1608455860306" TEXT="https://docs.qq.com/doc/DSmV3dUthSUtISHBa"/>
+</node>
+<node CREATED="1608434975083" ID="ID_512534180" MODIFIED="1608434984402" TEXT="&#x8865;&#x8d34;&#x5546;&#x54c1;&#x5217;&#x8868;">
+<node CREATED="1608456357218" ID="ID_319971401" MODIFIED="1608456357679" TEXT="https://docs.qq.com/doc/DSlJZQWRKVUlUcmln"/>
+</node>
+<node CREATED="1608454906539" ID="ID_1066474843" MODIFIED="1608454912490" TEXT="&#x65b0;&#x589e;&#x5408;&#x540c;&#x8865;&#x8d34;&#x6570;&#x636e;">
+<node CREATED="1608456370015" ID="ID_1657142054" MODIFIED="1608456371116" TEXT="https://docs.qq.com/doc/DSnNud2ZkeGNyQW5z"/>
+</node>
+</node>
+</node>
+</node>
+</map>
