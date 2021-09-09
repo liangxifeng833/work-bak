e7fff98050ad935fe3a365897fcc8cc5ccb99c41
liangxifeng <liangxifeng833@163.com>
2021-09-09 17:43:08 +0800

    add fengdinge disager

---
+++ mm/微信强券技术框架.mm
@@ -0,0 +1,6 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1622452488952" ID="ID_1677060407" MODIFIED="1622452501496" TEXT="&#x6280;&#x672f;&#x6846;&#x67b6;">
+<node CREATED="1622453598029" ID="ID_917139770" MODIFIED="1622453607113" POSITION="right" TEXT="springMVc"/>
+</node>
+</map>
---
+++ mm/领域驱动设计.mm
@@ -0,0 +1,8 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1623054162006" ID="ID_1651633011" MODIFIED="1623054172004" TEXT="&#x9886;&#x57df;&#x9a71;&#x52a8;&#x8bbe;&#x8ba1;">
+<node CREATED="1623054258312" ID="ID_1339330216" MODIFIED="1623054473019" POSITION="right" TEXT="&#x6846;&#x67b6;">
+<node CREATED="1623054260670" ID="ID_1388149895" MODIFIED="1623054263750" TEXT="springMvc"/>
+</node>
+</node>
+</map>
