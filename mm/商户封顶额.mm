d99b13b8a7c5118e50607610aff50cc8a8e02e5c
liangxifeng <liangxifeng833@163.com>
2021-09-11 17:39:57 +0800

    modify fengding value

--- mm/商户封顶额.mm
+++ mm/商户封顶额.mm
@@ -6,11 +6,12 @@
 <node CREATED="1631158321131" ID="ID_535376168" MODIFIED="1631158331984" TEXT="&#x5c01;&#x9876;&#x989d;&#x8868;&#x76f8;&#x5173;api">
 <node CREATED="1631155762479" ID="ID_1928684480" MODIFIED="1631156201814" TEXT="&#x65b0;&#x589e;&#x5c01;&#x9876;&#x989d;&#x6d3b;&#x52a8; martCap/add"/>
 <node CREATED="1631156204978" ID="ID_154893607" MODIFIED="1631157254772" TEXT="&#x4fee;&#x6539;&#x5c01;&#x9876;&#x989d;&#x6d3b;&#x52a8; martCap/updateById"/>
-<node CREATED="1631156587994" ID="ID_694474442" MODIFIED="1631174185168" TEXT="&#x901a;&#x8fc7;&#x4e3b;&#x952e;&#x67e5;&#x8be2;&#x5c01;&#x9876;&#x989d;&#x6d3b;&#x52a8;&#x63a5;&#x53e3; martCap/getById"/>
+<node CREATED="1631156587994" ID="ID_694474442" MODIFIED="1631332258316" TEXT="&#x901a;&#x8fc7;&#x4e3b;&#x952e;&#x67e5;&#x8be2;&#x5c01;&#x9876;&#x989d;&#x6d3b;&#x52a8;&#x63a5;&#x53e3; martCap/getById">
+<node CREATED="1631332116768" ID="ID_317305262" MODIFIED="1631332117613" TEXT="https://docs.qq.com/doc/DSlJmRWV3eXF0Snlt"/>
+</node>
 <node CREATED="1631156745359" ID="ID_1354043171" MODIFIED="1631157267737" TEXT="&#x5c01;&#x9876;&#x989d;&#x6d3b;&#x52a8;&#x5217;&#x8868; martCap/getPage"/>
 </node>
 <node CREATED="1631158339788" ID="ID_210760330" MODIFIED="1631158344905" TEXT="&#x5173;&#x8054;&#x6279;&#x6b21;&#x8868;&#x76f8;&#x5173;api">
-<node CREATED="1631158346204" ID="ID_1072798813" MODIFIED="1631159284983" TEXT="&#x67e5;&#x8be2;&#x7535;&#x5b50;&#x5238;&#x6279;&#x6b21;&#xff1a;&#x67e5;&#x8be2;&#x6ca1;&#x6709;&#x5173;&#x8054;&#x6d3b;&#x52a8;&#x7684;&#x7535;&#x5b50;&#x5238;&#x5238;&#x6279;&#x6b21;&#x63a5;&#x53e3; getNoActivityTicketBatch"/>
 <node CREATED="1631177695849" ID="ID_1843750656" MODIFIED="1631177715471" TEXT="&#x65b0;&#x589e;&#x5c01;&#x9876;&#x989d;&#x6279;&#x6b21;&#x63a5;&#x53e3; capBatch/add"/>
 <node CREATED="1631178921269" ID="ID_87451604" MODIFIED="1631178922274" TEXT="&#x901a;&#x8fc7;&#x4e3b;&#x952e;&#x67e5;&#x8be2;&#x5c01;&#x9876;&#x989d;&#x6279;&#x6b21;&#x63a5;&#x53e3;">
 <node CREATED="1631178927899" ID="ID_818036420" MODIFIED="1631178928718" TEXT="capBatch/getById"/>
@@ -19,9 +20,60 @@
 <node CREATED="1631179293016" ID="ID_239366208" MODIFIED="1631179301841" TEXT="capBatch/updateById"/>
 </node>
 <node CREATED="1631177716903" ID="ID_1856830905" MODIFIED="1631178033720" TEXT="&#x5c01;&#x9876;&#x989d;&#x6279;&#x6b21;&#x5217;&#x8868;&#x63a5;&#x53e3; capBatch/getPage"/>
-<node CREATED="1631179272836" ID="ID_1800616563" MODIFIED="1631179275722" TEXT=""/>
+</node>
+<node CREATED="1631242500733" ID="ID_1603448258" MODIFIED="1631242508225" TEXT="&#x500d;&#x6570;&#x6a21;&#x578b;api">
+<node CREATED="1631242509328" ID="ID_379472080" MODIFIED="1631242519097" TEXT="&#x65b0;&#x589e;&#x500d;&#x6570;&#x6a21;&#x578b;&#x63a5;&#x53e3; multipleModel/add"/>
+<node CREATED="1631242520732" ID="ID_1486258692" MODIFIED="1631242543040" TEXT="&#x901a;&#x8fc7;&#x4e3b;&#x952e;&#x67e5;&#x8be2;&#x500d;&#x6570;&#x6a21;&#x578b;&#x63a5;&#x53e3;&#x3000;multipleModel/getById"/>
+<node CREATED="1631242679577" ID="ID_1628963378" MODIFIED="1631242685866" TEXT="&#x901a;&#x8fc7;&#x4e3b;&#x952e;&#x4fee;&#x6539;&#x500d;&#x6570;&#x6a21;&#x578b;&#x63a5;&#x53e3; multipleModel/updateById"/>
+<node CREATED="1631242724001" ID="ID_450318766" MODIFIED="1631243393798" TEXT="&#x500d;&#x6570;&#x6a21;&#x578b;&#x5217;&#x8868;&#x63a5;&#x53e3; multipleModel/getPage"/>
+<node CREATED="1631331191821" FOLDED="true" ID="ID_1131632238" MODIFIED="1631332121492" TEXT="&#x901a;&#x8fc7;&#x5c01;&#x9876;&#x989d;&#x6d3b;&#x52a8;&#x4e3b;&#x952e;&#x67e5;&#x8be2;&#x500d;&#x6570;&#x6a21;&#x578b;&#x63a5;&#x53e3; mutipleModel/getByCapId">
+<node CREATED="1631331199904" ID="ID_1412317811" MODIFIED="1631331215755" TEXT="https://docs.qq.com/doc/DSnZvSlJGTmNSekpa"/>
 </node>
 </node>
-<node CREATED="1631158316251" ID="ID_1891516854" MODIFIED="1631158316251" POSITION="right" TEXT=""/>
+<node CREATED="1631244236807" ID="ID_681664990" MODIFIED="1631244248115" TEXT="&#x5c01;&#x9876;&#x989d;&#x5546;&#x6237;api">
+<node CREATED="1631244815092" ID="ID_1996656963" MODIFIED="1631254965368" TEXT="&#x6279;&#x91cf;&#x751f;&#x6210;&#x5546;&#x6237;&#x5c01;&#x9876;&#x989d;&#x63a5;&#x53e3; capMerchant/batchCreateCap"/>
+<node CREATED="1631259190019" FOLDED="true" ID="ID_599472952" MODIFIED="1631260172480" TEXT="&#x9009;&#x62e9;&#x5c01;&#x9876;&#x989d;&#x5546;&#x6237;&#x5217;&#x8868;&#x63a5;&#x53e3; capMerchant/srcMerchantList">
+<node CREATED="1631260170417" ID="ID_503085534" MODIFIED="1631260171070" TEXT="https://docs.qq.com/doc/DSnhhcnFDRFBJYkxz"/>
+</node>
+<node CREATED="1631262176899" FOLDED="true" ID="ID_355076717" MODIFIED="1631262207371" TEXT="&#x65b0;&#x589e;&#x5c01;&#x9876;&#x989d;&#x5546;&#x6237;&#x63a5;&#x53e3; capMerchant/add">
+<node CREATED="1631262204306" ID="ID_929639619" MODIFIED="1631262205096" TEXT="https://docs.qq.com/doc/DSnhhcnFDRFBJYkxz"/>
+</node>
+<node CREATED="1631264234937" FOLDED="true" ID="ID_1556457572" MODIFIED="1631332122910" TEXT="&#x5c01;&#x9876;&#x989d;&#x5546;&#x6237;&#x540d;&#x5355;&#x5217;&#x8868;&#x63a5;&#x53e3; capMerchant/getPage">
+<node CREATED="1631264240944" ID="ID_1428002284" MODIFIED="1631264249474" TEXT="https://docs.qq.com/doc/DSnZvSlJGTmNSekpa"/>
+</node>
+<node CREATED="1631265038701" FOLDED="true" ID="ID_395408290" MODIFIED="1631332123727" TEXT="&#x5220;&#x9664;&#x5c01;&#x9876;&#x989d;&#x5546;&#x6237;&#x63a5;&#x53e3; capMerchant/deleteGyId">
+<node CREATED="1631265061293" ID="ID_1819094653" MODIFIED="1631265061753" TEXT="https://docs.qq.com/doc/DSnZvSlJGTmNSekpa"/>
+</node>
+</node>
+<node CREATED="1631259722780" ID="ID_276262822" MODIFIED="1631260071423" TEXT="&#x516c;&#x5171;&#x63a5;&#x53e3; &#x662f;&#x5426;&#x9700;&#x8981;&#x5355;&#x72ec;&#x8bbe;&#x8ba1;&#xff1f;&#xff1f;&#xff1f;">
+<icon BUILTIN="yes"/>
+<node CREATED="1631259733418" ID="ID_1152755290" MODIFIED="1631332232936" TEXT="&#x5c55;&#x4f4d;&#x533a;&#x57df;&#x5217;&#x8868;&#x63a5;&#x53e3; showAreaList ">
+<node CREATED="1631259753683" ID="ID_615419343" MODIFIED="1631260039508" TEXT="https://docs.qq.com/doc/DSnhhcnFDRFBJYkxz"/>
+</node>
+<node CREATED="1631158346204" ID="ID_1072798813" MODIFIED="1631159284983" TEXT="&#x67e5;&#x8be2;&#x7535;&#x5b50;&#x5238;&#x6279;&#x6b21;&#xff1a;&#x67e5;&#x8be2;&#x6ca1;&#x6709;&#x5173;&#x8054;&#x6d3b;&#x52a8;&#x7684;&#x7535;&#x5b50;&#x5238;&#x5238;&#x6279;&#x6b21;&#x63a5;&#x53e3; getNoActivityTicketBatch"/>
+</node>
+</node>
+<node CREATED="1631158316251" ID="ID_1891516854" MODIFIED="1631353141114" POSITION="right" TEXT="&#x95ee;&#x9898;">
+<icon BUILTIN="help"/>
+<node CREATED="1631347006978" ID="ID_914355912" MODIFIED="1631347103738" TEXT="&#x5f53;&#x73ed;&#x7edf;&#x8ba1;">
+<node CREATED="1631346849981" ID="ID_755039190" MODIFIED="1631346888450" TEXT="&#x5f53;&#x73ed;&#x7edf;&#x8ba1;&#x7684;&#x662f;,&#x5148;&#x901a;&#x8fc7;&#x652f;&#x4ed8;&#x65b9;&#x5f0f;&#x67e5;&#x8be2;&#x6240;&#x6709;&#x652f;&#x4ed8;&#x65b9;&#x5f0f;"/>
+<node CREATED="1631346888933" ID="ID_1335548718" MODIFIED="1631346917163" TEXT="&#x800c;&#x6211;&#x4eec;&#x7535;&#x5b50;&#x5238;&#x6536;&#x6b3e;&#x540e;&#x8fdb;&#x5165;&#x4e86;payform_list&#x8868;&#x7684;">
+<node CREATED="1631346923564" ID="ID_1512635686" MODIFIED="1631346948785" TEXT="payform_trade &#x7535;&#x5b50;&#x5238;&#x91d1;&#x989d;"/>
+<node CREATED="1631346924772" ID="ID_576753013" MODIFIED="1631346957944" TEXT="payform_trade_fee &#x5546;&#x6237;&#x5206;&#x644a;&#x989d;"/>
+</node>
+<node CREATED="1631346962274" ID="ID_1251108999" MODIFIED="1631347004398" TEXT="&#x9664;&#x975e;&#x76f4;&#x63a5;&#x67e5;&#x8be2;db_ticket&#x7684;&#x6536;&#x5238;&#x8bb0;&#x5f55;&#x8868; ticket_pay_record, &#x901a;&#x8fc7;&#x5408;&#x540c;&#x53f7;&#x5173;&#x8054;"/>
+</node>
+<node CREATED="1631347104974" ID="ID_196617047" MODIFIED="1631347208370" TEXT="mis-&#x9500;&#x552e;&#x51ed;&#x8bc1;&#x5ba1;&#x6838;&#x660e;&#x7ec6; &#x95ee;&#x9898;&#x4e0e;&#x5f53;&#x73ed;&#x7edf;&#x8ba1;&#x4e00;&#x6837;, &#x56e0;&#x4e3a;&#x7535;&#x5b50;&#x5238;&#x6536;&#x6b3e;&#x540e;&#x6536;&#x5230;&#x4e86; payform_list&#x8868;&#x4e2d;"/>
+<node CREATED="1631349881136" ID="ID_1620848395" MODIFIED="1631349889541" TEXT="&#x6536;&#x94f6;&#x76f8;&#x5173;">
+<node CREATED="1631349890488" ID="ID_1442493018" MODIFIED="1631352733778" TEXT="&#x652f;&#x4ed8;&#x65b9;&#x5f0f;&#x67e5;&#x8be2;&#x5173;&#x4e8e;&#x7535;&#x5b50;&#x5238;&#x90e8;&#x5206; &#x4fee;&#x6539;&#x539f;&#x6709;&#x7a0b;&#x5e8f;: ">
+<node CREATED="1631350512148" ID="ID_615158323" MODIFIED="1631350512774" TEXT="https://docs.qq.com/doc/DSmJKYU53T09TbWJh"/>
+<node CREATED="1631350554988" ID="ID_1825205974" MODIFIED="1631350580199" TEXT="php&#x90e8;&#x5206;&#x662f;&#x524d;&#x7aef;&#x8be5;&#x8fd8;&#x662f;&#x540e;&#x7aef;&#x6539;."/>
+</node>
+<node CREATED="1631352718581" ID="ID_958547794" MODIFIED="1631352749458" TEXT="&#x5546;&#x6237;&#x5bf9;&#x8d26;&#x652f;&#x4ed8;&#x660e;&#x7ec6;">
+<node CREATED="1631352757660" ID="ID_794162904" MODIFIED="1631352758456" TEXT="http://192.168.9.157:8009/unconfirmed_bill_detail.php?startDate=&amp;endDate=&amp;offset=0&amp;id=1975211&amp;num=151202185"/>
+<node CREATED="1631352759187" ID="ID_1664486279" MODIFIED="1631352787889" TEXT="&#x53d1;&#x73b0;&#x6ca1;&#x6709;&#x5c06;&#x7535;&#x5b50;&#x5238;&#x7684;&#x624b;&#x7eed;&#x8d39;&#x8ba1;&#x7b97;&#x51fa;&#x6765;"/>
+</node>
+</node>
+</node>
 </node>
 </map>
